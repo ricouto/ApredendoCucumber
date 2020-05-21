5abae16236bbf600eb131732fb111be8db9a8804
ricouto <ricardocaputo@gmail.com>
2020-05-21 09:30:51 -0300

    inclusao de novas classes

---
+++ src/main/java/cucumber/servicos/AluguelService.java
@@ -0,0 +1,20 @@
+package cucumber.servicos;
+
+import java.util.Calendar;
+
+import cucumber.entidades.Filme;
+import cucumber.entidades.NotaAluguel;
+
+public class AluguelService {
+	
+	public NotaAluguel alugar(Filme filme) {
+		NotaAluguel nota = new NotaAluguel();
+		nota.setPreco(filme.getAluguel());
+		
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		nota.setDataEntrega(cal.getTime());
+		filme.setEstoque(filme.getEstoque() - 1);
+		return nota;
+	}
+}
--- src/test/java/cucumber/runners/RunnerCucumber.java
+++ src/test/java/cucumber/runners/RunnerCucumber.java
@@ -7,12 +7,13 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/features/aprendercucumber.feature",
+		features = "src/test/resources/features/alugarFilme.feature",
 		glue = "cucumber.steps",
 		//tags = "~@ignore",
 		//tags = "@tipo1",
 		//tags = "@tipo2",
-		tags = {"@tipo1, @tipo2"},
+		//tags = {"@tipo1, @tipo2"},
+		tags = {},
 		plugin = "pretty",
 		monochrome = true,
 		snippets = SnippetType.CAMELCASE,
